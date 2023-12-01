a8befdcd54af45fe202c5a5a8c09e664b3da5a45
oflondon2012 <gtadebayo2023@gmail.com>
2023-12-01 04:53:56 +0000

    advance python-import_modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,8 +2,8 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if (a < b):
-        c = add(a, b)`
-        for count in range (4, 6):
+        c = add(a, b)
+        for count in range(4, 6):
             c = add(c, count)
         return (c)
     else:
