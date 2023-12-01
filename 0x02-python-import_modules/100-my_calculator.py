788f69305ba009fc0ac94877ba1bab1077600d62
oflondon2012 <gtadebayo2023@gmail.com>
2023-12-01 03:33:38 +0000

    advance python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if (__name__ == "__main__"):
+    arglen = len(argv)
+    ret = 0
+    operators = ["+", "-", "*", "/"]
+
+    if (arglen != 4):
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+
+    if (argv[2] not in operators):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    oprand = arg[2]
+
+    if (oprand == "+"):
+        ret = add(a, b)
+    elif (oprand == "-"):
+        ret = sub(a, b)
+    elif (oprand == "*"):
+        ret = mul(a, b)
+    else:
+        ret = div(a, b)
+    print(f"{a} {oprand} {b} = {ret}")
