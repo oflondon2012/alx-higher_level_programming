1594bab845348205dbcd1a79cf3cf74bb9bc71bc
oflondon2012 <gtadebayo2023@gmail.com>
2023-12-01 02:41:44 +0000

    python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from sys import argv
+
+def addarg(argv):
+    arglen = len(argv) - 1
+    if arglen == 0:
+        print("{:d}".format(arglen))
+        return
+    else:
+        count = 1
+        sum = 0
+        while count <= arglen:
+            sum += int(argv[count])
+            count += 1
+        print("{:d}".format(sum))
 
 if __name__ == "__main__":
-    count = 1
-    tsum = 0
-    arglen =  len(argv) - 1
+    import sys
+    addarg(sys.argv)
 
-    if (arglen > 0):
-        while (count <= arglen):
-            tsum += int(argv[count])
-            count += 1
-    print(tsum)
