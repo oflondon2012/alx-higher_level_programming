2423090995c10bae5d5bffd99f393a73458cf60b
oflondon2012 <gtadebayo2023@gmail.com>
2023-12-05 02:48:27 +0000

    last 2 task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info - function that print some basic python info
+ * @p: list of python
+ */
+void print_python_list_info(PyObject *p)
+{
+	int elist;
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (elist = 0; elist < Py_SIZE(p); elist++)
+	printf("Element %d: %s\n", elist, Py_TYPE(PyList_GetItem(p, elist))->tp_name);
+}
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,63 @@
+#include "list.h"
+#include <stdio.h>
+/**
+ * reverse_list - function that reverse a linked list
+ * @start: the begining of the list
+ *
+ * Return: the pointer to reserved list
+ */
+listint_t *reverse_list(listint_t **start)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+
+	while (*start != NULL)
+	{
+		next = (*start)->next;
+		(*start)->next = prev;
+		prev = *start;
+		*start - next;
+	}
+	return (prev);
+}
+/**
+ * is_palindrome - function that check if linked list is pal
+ * @start: the begining of list
+ *
+ * Return: (1) if palindrome else 0
+ */
+int is_palindrome(listint_t **start)
+{
+	listint_t *s_half, *fast = *start;
+	listint_t lotion = *start, *prev = *start;
+	int is_pal = 1;
+
+	if (*start != NULL && (*start)->next != NULL)
+	{
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev = lotion;
+			lotion = lotion->next;
+		}
+		if (fast != NULL)
+			lotion = lotion->next;
+		s_half = reverse_list(&lotion);
+		lotion = s_half;
+
+		while (lotion != NULL)
+		{
+			if ((*start)->n != lotion->n)
+			{
+				is_pal = 0;
+				break;
+			}
+			*start = (*start)->next;
+			lotion = lotion->next;
+		}
+
+		reverse_list(&s_half);
+		prev->next = s_half;
+	}
+	return (is_pal);
+}
