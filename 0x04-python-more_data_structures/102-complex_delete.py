5e8e4bdd09a5d50d2a01806a6b367f6e19e34d5a
oflondon2012 <gtadebayo2023@gmail.com>
2023-12-07 02:21:25 +0000

    Advance task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    tdic = my_dict.copy()
-    for i, j in tdic.items():
+    tmp = my_dict.copy()
+    for i, j in tmp.items():
         if value == j:
             my_dict.pop(i)
         return my_dict
