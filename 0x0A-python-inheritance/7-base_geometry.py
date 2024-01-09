a12c5e542d82ce9e7ba4b179aee89fac2f610efe
oflondon2012 <gtadebayo2023@gmail.com>
2024-01-09 00:41:27 +0000

    No test case needed

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,14 +2,16 @@
 """Module BaseGeometry which have 2 methods"""
 
 
-def area(self):
-    """Area not implemented at all"""
-    raise Exception("area() is not implemented")
+class BaseGeometry:
+    """Implement the properties"""
 
+    def area(self):
+        """Area not implemented at all"""
+        raise Exception("area() is not implemented")
 
-def integer_validator(self, name, value):
-    """validate integer value of 2 arguments"""
-    if not isinstance(value, int):
-        raise TypeError(f"{name} must be an integer")
-    if value <= 0:
-        raise ValueError(f"{name} must be greater than 0")
+    def integer_validator(self, name, value):
+        """validate integer value of 2 arguments"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
