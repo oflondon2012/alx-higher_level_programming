502d8ccb06f2399e3c2995f60bf7c31651653715
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-12 03:20:31 +0000

    Commiting the first 6 javascript task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const linemes = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let j = 0;
-for (j < linemes.length; j++) {
-	console.log(linemes[j]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
