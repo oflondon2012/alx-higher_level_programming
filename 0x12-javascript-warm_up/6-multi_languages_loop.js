91e91285542c4be5285b573be36fb061dcfb815f
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-12 04:20:36 +0000

    Commiting javascript task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const mess = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const mess = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let j = 0; j < mess.length; j++) {
   console.log(mess[j]);
 }
