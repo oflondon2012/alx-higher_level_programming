260be08471b1027c55afb18c7215394c9161540c
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-17 23:23:13 +0000

    python object relational mapping

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+    script that list state object from the
+    database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def state():
+    """Class function """
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+    db_host = "localhost"
+
+    try:
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@{}:3306/{}'
+                .format(db_user, db_pass, db_host, db_name),
+                pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        stat = session.query(State).filter(state.name.like("%a%"))
+        .order_by(State.id).all()
+
+        for state in stat
+        print("{}: {}".format(state.id, state.name))
+        session.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    state()
