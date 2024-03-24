959734d20b7fabba274235a4df8bcca49ea065c8
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-24 10:07:52 +0000

    update the tasks

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+    script that list state object from the
+    database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def state():
+    """Class function """
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+    db_host = "localhost"
+
+    try:
+        engine = create_engine(
+                'mysql+mysqldb://{}:{}@{}:3306/{}'
+                .format(db_user, db_pass, db_host, db_name),
+                pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        states = session.query(State).filter(State.id == 2).first()
+        if states:
+            states.name = "New Mexico"
+        session.commit()
+        session.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    state()
