cfc023f4fda25e84057ddb01a8e99eabeacbc501
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-29 05:20:52 +0000

    python network task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-'''Python script that fetches https://alx-intranet.hbtn.io/status'''
+'''
+script that takes in a URL, sends a request to the URL and displays
+the body of the response
+'''
 import requests
 import sys
 
