a711f4eb2cf9c39cc0c70cf4b2450f98dab39141
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-29 05:09:22 +0000

    python network task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,7 @@
 """
 script that takes in a URL and an email address, sends a POST request
 to the passed URL with the email as a parameter, and finally displays
-the body of the response.
+the body of the response
 """
 from sys import argv
 import requests
